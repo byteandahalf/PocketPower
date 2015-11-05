4382773e8a4d09ffb9dfa1c3f8238e8eb133ec42
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2015-11-05 11:24:32 -0600

    Delete RepeaterItem.h

--- jni/mcperedstone/world/item/RepeaterItem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "mcpe/world/item/Item.h"
-
-class RepeaterItem: public Item {
-public:
-	RepeaterItem(int);
-	
-	virtual bool useOn(ItemInstance*, Player*, int, int, int, signed char, float, float, float);
-};
